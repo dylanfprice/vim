ffcf62575a6e70756989471c0f173fab4330ef70
Dylan Price <the.dylan.price@gmail.com>
2016-02-23 14:13:44 -0800

    Removed esv folder.

--- esv/generate_tags.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-ctags -f $ENERGYSAVVY_DIR/package-tags -R $ENERGYSAVVY_DIR/energysavvy_venv/lib/python2.7/ 2>/dev/null 
-ctags --exclude='node_modules' --exclude='migrations' -f $ENERGYSAVVY_DIR/optix-tags -R $ENERGYSAVVY_DIR/mainrepo/ 2>/dev/null 
